3b58f2e4858f41e3ddb105dae96e73c955381f6e
Rafal <rafalmiedziocha@iMac.local>
2016-04-22 20:29:43 +0200

    Master: Basic homepage

---
+++ views/site/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Home Finance</h1>
+
+        <p class="lead">The best way to manage your home finance.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Create your wallet</a></p>
+    </div>
+
+    <div class="body-content">
+
+    </div>
+</div>
