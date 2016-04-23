8fbb7194805e78f967e8fe894535fdc735ba06b2
Rafal <rafalmiedziocha@iMac.local>
2016-04-23 15:02:01 +0200

    Master: Default error view.

---
+++ views/site/error.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+$this->title = $name;
+?>
+<div class="site-error">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="alert alert-danger">
+        <?= nl2br(Html::encode($message)) ?>
+    </div>
+
+    <p>
+        The above error occurred while the Web server was processing your request.
+    </p>
+    <p>
+        Please contact us if you think this is a server error. Thank you.
+    </p>
+
+</div>
