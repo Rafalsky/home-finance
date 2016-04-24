d60b284f392be9f5eb22f210c5629a6e43f89615
Rafal <rafalmiedziocha@iMac.local>
2016-04-24 18:14:22 +0200

    Master: Backend assets

---
+++ backend/assets/BackendAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+class BackendAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+
+    public $css = [
+        'css/style.css'
+    ];
+    public $js = [
+        'js/app.js'
+    ];
+
+    public $depends = [
+        \yii\web\YiiAsset::class,
+        \common\assets\AdminLte::class,
+        \common\assets\Html5shiv::class
+    ];
+}
