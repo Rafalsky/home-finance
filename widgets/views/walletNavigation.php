20eebeff2996717487f6445818e9b9e8f33a56cf
Rafal <rafalmiedziocha@iMac.local>
2016-04-24 18:15:28 +0200

    Master: Improve wallet navigation

--- widgets/views/walletNavigation.php
+++ widgets/views/walletNavigation.php
@@ -14,24 +14,60 @@ echo SideNav::widget([
     'type' => SideNav::TYPE_WARNING,
     'items' => [
         [
-            'label' => Yii::t('wallet', 'Home'),
+            'label' => Yii::t('app', 'Home'),
             'url' => ['/wallet'],
             'icon' => 'home'
         ],
         [
-            'label' => Yii::t('wallet', 'Transactions'),
+            'label' => Yii::t('app', 'Transactions'),
             'icon' => 'question-sign',
             'items' => [
 
                 [
-                    'label' => Yii::t('wallet', 'List'),
+                    'label' => Yii::t('app', 'List'),
                     'url' => ['/wallet/transactions/list'],
                     'icon' => 'question-sign',
                 ],
 
                 [
-                    'label' => Yii::t('wallet', 'Add Receipt'),
+                    'label' => Yii::t('app', 'Add Receipt'),
                     'url' => ['/wallet/transactions/add-receipt'],
+                    'icon' => 'question-sign',
+                ],
+            ],
+        ],
+        [
+            'label' => Yii::t('app', 'Shops'),
+            'icon' => 'question-sign',
+            'items' => [
+
+                [
+                    'label' => Yii::t('app', 'List'),
+                    'url' => ['/wallet/shop/list'],
+                    'icon' => 'question-sign',
+                ],
+
+                [
+                    'label' => Yii::t('app', 'New'),
+                    'url' => ['/wallet/shop/new'],
+                    'icon' => 'question-sign',
+                ],
+            ],
+        ],
+        [
+            'label' => Yii::t('app', 'Companies'),
+            'icon' => 'question-sign',
+            'items' => [
+
+                [
+                    'label' => Yii::t('app', 'List'),
+                    'url' => ['/wallet/company/list'],
+                    'icon' => 'question-sign',
+                ],
+
+                [
+                    'label' => Yii::t('app', 'New'),
+                    'url' => ['/wallet/company/new'],
                     'icon' => 'question-sign',
                 ],
             ],
