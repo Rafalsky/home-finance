432cc37a09c4d3427275e0dace460d27d00b12ab
Rafal <admin@rafalsky.com>
2016-05-10 08:21:18 +0200

    Master: Error action for wallet module

---
+++ wallet/controllers/SiteController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ *  This file is part of the HomeFinanceV2 project.
+ *
+ *  (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace wallet\controllers;
+
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ]
+        ];
+    }
+}
