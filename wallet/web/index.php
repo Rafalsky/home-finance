d43cd64d450f28911f8b2ca70bd0fa94966481c5
Rafal <admin@rafalsky.com>
2016-05-12 23:13:19 +0200

    Master: Wallet web folder

---
+++ wallet/web/index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Composer
+require(__DIR__ . '/../../vendor/autoload.php');
+
+// Environment
+require(__DIR__ . '/../../common/env.php');
+
+// Yii
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+
+// Bootstrap application
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../config/bootstrap.php');
+
+
+$config = \yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/base.php'),
+    require(__DIR__ . '/../../common/config/web.php'),
+    require(__DIR__ . '/../config/base.php'),
+    require(__DIR__ . '/../config/web.php')
+);
+
+(new yii\web\Application($config))->run();
---
+++ wallet/web/js/app.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$(function() {
+    "use strict";
+
+    //Make the dashboard widgets sortable Using jquery UI
+    $(".connectedSortable").sortable({
+        placeholder: "sort-highlight",
+        connectWith: ".connectedSortable",
+        handle: ".box-header, .nav-tabs",
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    }).disableSelection();
+    $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+});
