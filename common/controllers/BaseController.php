a5309244fa65e414e3ca280d4f6c9106f8f38d44
Rafal <admin@rafalsky.com>
2016-05-13 21:44:20 +0200

    Master: BaseController

---
+++ common/controllers/BaseController.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace common\controllers;
+
+use common\models\base\BaseModel;
+use yii\web\Controller;
+
+abstract class BaseController extends Controller
+{
+    protected function updateWithPostRequest(BaseModel $model)
+    {
+        if (\Yii::$app->request->isPost && $attributes = \Yii::$app->request->post($model->getShortClassName())) {
+            $model->attributes = $attributes;
+            if ($model->save()) {
+                \Yii::$app->session->addFlash('success', $this->getSuccessMessage());
+                $this->afterSave();
+            } else {
+                \Yii::$app->session->addFlash('success', $this->getFailedMessage());
+            }
+        }
+        return $model;
+    }
+
+    protected function getSuccessMessage()
+    {
+        return \Yii::t('app', 'Model has been saved');
+    }
+
+    protected function getFailedMessage()
+    {
+        return \Yii::t('app', 'Error while saving model');
+    }
+
+    protected function afterSave()
+    {
+    }
+}
