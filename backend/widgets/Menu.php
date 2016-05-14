dda9372c82ec13e1c1f19fe404d6db9cf5ea5210
Rafal <admin@rafalsky.com>
2016-05-14 17:44:35 +0200

    Master: Backend menu widget and command

---
+++ backend/widgets/Menu.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace backend\widgets;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/**
+ * Class Menu
+ * @package backend\components\widget
+ */
+class Menu extends \yii\widgets\Menu
+{
+    /**
+     * @var string
+     */
+    public $linkTemplate = "<a href=\"{url}\">\n{icon}\n{label}\n{right-icon}\n{badge}</a>";
+    /**
+     * @var string
+     */
+    public $labelTemplate = "{icon}\n{label}\n{badge}";
+
+    /**
+     * @var string
+     */
+    public $badgeTag = 'span';
+    /**
+     * @var string
+     */
+    public $badgeClass = 'label pull-right';
+    /**
+     * @var string
+     */
+    public $badgeBgClass;
+
+    /**
+     * @var string
+     */
+    public $parentRightIcon = '<i class="fa fa-angle-left pull-right"></i>';
+
+    /**
+     * @inheritdoc
+     * @throws \yii\base\InvalidParamException
+     */
+    protected function renderItem($item)
+    {
+        $item['badgeOptions'] = isset($item['badgeOptions']) ? $item['badgeOptions'] : [];
+
+        if (!ArrayHelper::getValue($item, 'badgeOptions.class')) {
+            $bg = isset($item['badgeBgClass']) ? $item['badgeBgClass'] : $this->badgeBgClass;
+            $item['badgeOptions']['class'] = $this->badgeClass . ' ' . $bg;
+        }
+
+        if (isset($item['items']) && !isset($item['right-icon'])) {
+            $item['right-icon'] = $this->parentRightIcon;
+        }
+
+        if (isset($item['url'])) {
+            $template = ArrayHelper::getValue($item, 'template', $this->linkTemplate);
+
+            return strtr($template, [
+                '{badge}'=> isset($item['badge'])
+                    ? Html::tag('small', $item['badge'], $item['badgeOptions'])
+                    : '',
+                '{icon}'=>isset($item['icon']) ? $item['icon'] : '',
+                '{right-icon}'=>isset($item['right-icon']) ? $item['right-icon'] : '',
+                '{url}' => Url::to($item['url']),
+                '{label}' => $item['label'],
+            ]);
+        } else {
+            $template = ArrayHelper::getValue($item, 'template', $this->labelTemplate);
+
+            return strtr($template, [
+                '{badge}'=> isset($item['badge'])
+                    ? Html::tag('small', $item['badge'], $item['badgeOptions'])
+                    : '',
+                '{icon}'=>isset($item['icon']) ? $item['icon'] : '',
+                '{right-icon}'=>isset($item['right-icon']) ? $item['right-icon'] : '',
+                '{label}' => $item['label'],
+            ]);
+        }
+    }
+}
