86848553af13670008455a3fbf5a9d1010bd1f4d
Rafal <admin@rafalsky.com>
2016-05-14 17:57:44 +0200

    Master: Console module

---
+++ console/config/console.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'id' => 'console',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'console\controllers',
+    'controllerMap' => [
+        'command-bus' => [
+            'class' => 'trntv\bus\console\BackgroundBusController',
+        ],
+        'message' => [
+            'class' => 'console\controllers\ExtendedMessageController'
+        ],
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            'migrationPath' => '@common/migrations/db',
+            'migrationTable' => '{{%system_db_migration}}'
+        ],
+        'rbac-migrate' => [
+            'class' => 'console\controllers\RbacMigrateController',
+            'migrationPath' => '@common/migrations/rbac/',
+            'migrationTable' => '{{%system_rbac_migration}}',
+            'templateFile' => '@common/rbac/views/migration.php'
+        ],
+    ],
+];
---
+++ console/controllers/RbacMigrateController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the HomeFinanceV2 project.
+ *
+ * (c) Rafalsky.com <http://github.com/Rafalsky/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace console\controllers;
+
+use yii\console\controllers\MigrateController;
+
+/**
+ * @author Eugene Terentev <eugene@terentev.net>
+ */
+class RbacMigrateController extends MigrateController
+{
+    /**
+     * Creates a new migration instance.
+     * @param string $class the migration class name
+     * @return \common\rbac\Migration the migration instance
+     */
+    protected function createMigration($class)
+    {
+        $file = $this->migrationPath . DIRECTORY_SEPARATOR . $class . '.php';
+        require_once($file);
+
+        return new $class();
+    }
+}
