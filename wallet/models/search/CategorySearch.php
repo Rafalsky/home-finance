c1a3355fa7f216043fb3d6e50be5fa3d7847b63c
Rafal <admin@rafalsky.com>
2016-06-29 20:30:58 +0200

    Master: Add CategorySearch class

---
+++ wallet/models/search/CategorySearch.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace wallet\models\search;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Category;
+
+/**
+ * CategorySearch represents the model behind the search form about `common\models\Category`.
+ */
+class CategorySearch extends Category
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'type'], 'integer'],
+            [['name', 'created_at', 'updated_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Category::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'type' => $this->type,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name]);
+
+        return $dataProvider;
+    }
+}
